1e75a11d4b8d528c4f65e3b23ab8e13e3db7afcd
Aarav Arora <aaravarora.works@gmail.com>
2023-02-18 23:49:19 +0530

    fix: docker fixes

--- Task_1/config/config.go
+++ Task_1/config/config.go
@@ -8,7 +8,7 @@ import (
 
 func Load() error {
 	test := os.Getenv("PORT") // checking if the env variables are already loaded or not
-	if test != "" {
+	if test == "" {
 		err := godotenv.Load() // loading the .env file
 		if err != nil {
 			return err
