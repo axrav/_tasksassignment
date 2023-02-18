50a064ebf994621e97e15c5c93989f51b99b1ef0
Aarav Arora <aaravarora.works@gmail.com>
2023-02-18 23:33:35 +0530

    feat: dockerizing the application

--- Task_1/config/config.go
+++ Task_1/config/config.go
@@ -1,11 +1,18 @@
 package config
 
-import "github.com/joho/godotenv"
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func Load() error {
-	err := godotenv.Load() // loading the .env file
-	if err != nil {
-		return err
+	test := os.Getenv("PORT") // checking if the env variables are already loaded or not
+	if test != "" {
+		err := godotenv.Load() // loading the .env file
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
